4d81e1cdf4bde2f33d2766287816b3d693e4b58c
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-01-23 16:11:09 +0530

    Time: 3 ms (87.67%) | Memory: 43.2 MB (79.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+    Stack<Character> st=new Stack<>();
+    int n=s.length();
+    for(int i=0;i<n;i++)
+    {
+        char ch=s.charAt(i);
+        if(ch=='('||ch=='{'||ch=='[')
+        {
+            st.push(ch);
+        }
+        else{
+            if(st.isEmpty())
+            {
+                return false;
+            }
+            char check=st.pop();
+            if(ch==')'&&check!='(')
+            {
+                return false;
+            }
+            else if(ch=='}'&&check!='{')
+            {
+                return false;
+            }
+            else if(ch==']'&&check!='[')
+            {
+                return false;
+            }
+        }
+    }
+    return st.isEmpty();
+    }
+}
