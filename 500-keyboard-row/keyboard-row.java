096baa46e395735caa9cb2276cef060024cc76cb
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-02-11 14:34:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (47.82%) - LeetSync

--- 500-keyboard-row/keyboard-row.java
+++ 500-keyboard-row/keyboard-row.java
@@ -1,22 +1,7 @@
 class Solution {
     public String[] findWords(String[] words) {
-    String f1="qwertyuiopQWERTYUIOP",f2="asdfghjklASDFGHJKL",f3="zxcvbnmZXCVBNM";
-    ArrayList<Character> first=new ArrayList<>();
-    for(int i=0;i<20;i++)
-    {
-        first.add(f1.charAt(i));
-    }
-    ArrayList<Character> second=new ArrayList<>();
-    for(int i=0;i<18;i++)
-    {
-        second.add(f2.charAt(i));
-    }
-    ArrayList<Character> third=new ArrayList<>();
-    for(int i=0;i<14;i++)
-    {
-      third.add(f3.charAt(i));
-    }
     ArrayList<String> arr=new ArrayList<>();
+    String f1="qwertyuiopQWERTYUIOP",f2="asdfghjklASDFGHJKL",f3="zxcvbnmZXCVBNM";
     int n=words.length;
     for(int i=0;i<n;i++)
     {
@@ -24,15 +9,16 @@ class Solution {
         boolean check1=true,check2=true,check3=true;
         for(int j=0;j<word.length();j++)
         {
-            if(!first.contains(word.charAt(j)))
-            {
+             String ch=Character.toString(word.charAt(j));
+            if(!f1.contains(ch))
+            {            
                 check1=false;
             }
-            if(!second.contains(word.charAt(j)))
+            if(!f2.contains(ch))
             {
                 check2=false;
             }
-            if(!third.contains(word.charAt(j)))
+            if(!f3.contains(ch))
             {
                 check3=false;
             }
