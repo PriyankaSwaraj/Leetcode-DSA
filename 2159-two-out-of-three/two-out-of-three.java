3de1737023354f971b0ed48bc5d8f479f510f097
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-02-25 22:52:12 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (87.03%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.java
+++ 2159-two-out-of-three/two-out-of-three.java
@@ -1,41 +1,27 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
-     boolean[] freq=new boolean[101];
+     boolean[] freq1=new boolean[101];
+     boolean[] freq2=new boolean[101];
+     boolean[] freq3=new boolean[101];
      List<Integer> arr=new ArrayList<>();
      for(int num:nums1)
      {
-        freq[num]=true;
+        freq1[num]=true;
      }
-     HashSet<Integer> set=new HashSet<>();
      for(int num:nums2)
      {
-        if(!set.contains(num))
-        {
-            if(freq[num])
-            {
-                arr.add(num);
-            }
-            else{
-                freq[num]=true;
-            }
-        }
-        set.add(num);
+        freq2[num]=true;
      }
-     set.clear();
      for(int num:nums3)
      {
-        if(!set.contains(num))
-        {
-            if(freq[num])
-            {
-                if(!arr.contains(num))
-                arr.add(num);
-            }
-            else{
-                freq[num]=true;
-            }
-        }
-        set.add(num);
+        freq3[num]=true;
+     }
+     for(int i=1;i<101;i++)
+     {
+      if(freq1[i]&&freq2[i]||freq2[i]&&freq3[i]||freq1[i]&&freq3[i])
+      {
+        arr.add(i);
+      }
      }
      return arr;
     }
