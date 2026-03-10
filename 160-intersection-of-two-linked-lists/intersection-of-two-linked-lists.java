b135ed446f5398442841ebb0e519ec6ad13e6051
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-03-10 12:05:10 +0530

    Time: 1 ms (99.89%) | Memory: 52.7 MB (77.60%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -13,34 +13,11 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
     ListNode temp1=headA;
     ListNode temp2=headB;
-    int len1=size(headA);
-    int len2=size(headB);
-    while(len2>len1)
-    {
-        temp2 = temp2.next;
-        len2--;
-    }
-    while(len1>len2)
-    {
-        temp1=temp1.next;
-        len1--;
-    }
     while(temp1!=temp2)
     {
-        temp1=temp1.next;
-        temp2=temp2.next;
-    }
-    return temp1;
-    }
-    public int size(ListNode head)
-    {
-        ListNode curr=head;
-        int size=0;
-        while(curr!=null)
-        {
-            size++;
-            curr=curr.next;
-        }
-        return size;
+        temp1=temp1==null?headB:temp1.next;
+        temp2=temp2==null?headA:temp2.next;
+    }    
+     return temp1;   
     }
 }
