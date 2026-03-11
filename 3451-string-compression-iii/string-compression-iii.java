c2a433f067334099958fa7a2b7b1a34910dc4690
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-03-11 12:01:33 +0530

    Time: 21 ms (56.91%) | Memory: 48.1 MB (98.71%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public String compressedString(String word) {
     int count=1,n=word.length();
-    String newStr="";
+    StringBuilder sb=new StringBuilder();
     for(int i=1;i<n;i++)
     {
         char ch=word.charAt(i-1);
         if(word.charAt(i)!=ch||count==9)
         {
-            newStr += Integer.toString(count);
-            newStr += Character.toString(ch);;
+            sb.append(count);
+            sb.append(ch);
             count =0;
         }
         count++;
     }
-    newStr += Integer.toString(count)+Character.toString(word.charAt(n-1));
-    return newStr;    
+    sb.append(count);
+    sb.append(word.charAt(n-1));
+    return sb.toString();    
     }
 }
