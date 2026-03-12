edc31ff755fe1ef9e7aaf72e890c7abb167d4a4f
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-03-12 14:53:17 +0530

    Time: 3 ms (73.96%) | Memory: 53.7 MB (47.67%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,26 +1,20 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-     int[] freq=new int[1001];
-     HashSet<Integer> set=new HashSet<>();
-     int m=trust.length;
-     if(m==0&&n==1)
+     int[] judge=new int[n+1];
+     for(int[] people:trust)
      {
-        return 1;
+        int a=people[0];
+        int b=people[1];
+        judge[a]--;
+        judge[b]++;
      }
-     for(int i=0;i<m;i++)
+     for(int i=1;i<=n;i++)
      {
-        freq[trust[i][1]]++;
-        set.add(trust[i][0]);
-     }   
-     ArrayList<Integer> arr=new ArrayList<>();
-     for(int i=0;i<m;i++)
-     {
-        int num=trust[i][1];
-        if(freq[num]==n-1&&!set.contains(num))
-        {
-            return num;
-        }
+       if(judge[i]==n-1)
+       {
+        return i;
+       }
      }
-    return -1;
+     return -1;
     }
 }
