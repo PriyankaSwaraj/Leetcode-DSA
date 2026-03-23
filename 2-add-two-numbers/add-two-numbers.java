d1b8d90a3f458eaf98e0ade3d28cf96e07aa7c9d
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-03-23 15:20:02 +0530

    Time: 1 ms (99.91%) | Memory: 46.7 MB (31.77%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,50 +10,26 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode head1=l1,head2=l2;
-    ArrayList<Integer> nums1=new ArrayList<>();
-    ArrayList<Integer> nums2=new ArrayList<>();
-    while(head1!=null)
+    ListNode dummy=new ListNode(0);
+    ListNode current=dummy;
+    int carry=0;
+    while(l1!=null||l2!=null||carry!=0)
     {
-        nums1.add(head1.val);
-        head1=head1.next;
+        int sum=carry;
+        if(l1!=null)
+        {
+            sum += l1.val;
+            l1=l1.next;
+        }
+        if(l2!=null)
+        {
+            sum += l2.val;
+            l2=l2.next;
+        }
+        carry=sum/10;
+        current.next=new ListNode(sum%10);
+        current=current.next;
     }
-    while(head2!=null)
-    {
-        nums2.add(head2.val);
-        head2=head2.next;
-    }
-    ArrayList<Integer> result=new ArrayList<>();
-    int n=nums1.size(),m=nums2.size();
-    int carry=0,i;
-    for(i=0;i<n&&i<m;i++)
-    {
-        int num=nums1.get(i)+nums2.get(i)+carry;
-        result.add(num%10);
-        carry=num/10;
-    }
-    for(int j=i;j<n;j++)
-    {
-        int num=nums1.get(j)+carry;
-        result.add(num%10);
-        carry=num/10;
-    }
-    for(int j=i;j<m;j++)
-    {
-        int num=nums2.get(j)+carry;
-        result.add(num%10);
-        carry=num/10;
-    }
-    if(carry>0)
-    result.add(carry);
-    l1.val=result.get(0);
-    ListNode temp=l1;
-    int k=result.size();
-    for(int j=1;j<k;j++)
-    {
-        temp.next=new ListNode(result.get(j));
-        temp=temp.next;
-    }
-    return l1;
+    return dummy.next;
     }
 }
