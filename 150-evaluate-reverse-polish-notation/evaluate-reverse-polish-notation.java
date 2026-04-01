10bc38c373661314caaec5cadd631756edc1c680
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-04-01 14:43:49 +0530

    Time: 6 ms (91.11%) | Memory: 45.4 MB (37.55%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,32 +2,30 @@ class Solution {
     public int evalRPN(String[] tokens) {
     int n=tokens.length;
     Deque<Integer> stack=new ArrayDeque<>();
-    for(int i=0;i<n;i++)
+   for(String str:tokens)
+   {
+    if(str.equals("+"))
     {
-        String str=tokens[i];
-        if(str.matches(".*\\d.*"))
-        {
-            stack.push(Integer.parseInt(str));
-        }
-        else{
-            int op1=stack.pop(),op2=stack.pop();
-            if(str.equals("+"))
-            {
-                stack.push(op1+op2);
-            }
-            else if(str.equals("-"))
-            {
-                stack.push(op2-op1);
-            }
-            else if(str.equals("*"))
-            {
-                stack.push(op1*op2);
-            }
-            else{
-                stack.push(op2/op1);
-            }
-        }
-    } 
+        stack.push(stack.pop()+stack.pop());
+    }
+    else if(str.equals("-"))
+    {
+        int op1=stack.pop(),op2=stack.pop();
+        stack.push(op2-op1);
+    }
+    else if(str.equals("*"))
+    {
+        stack.push(stack.pop()*stack.pop());
+    }
+    else if(str.equals("/"))
+    {
+        int op1=stack.pop(),op2=stack.pop();
+        stack.push(op2/op1);
+    }
+    else{
+        stack.push(Integer.parseInt(str));
+    }
+   }
     return stack.pop();   
     }
 }
