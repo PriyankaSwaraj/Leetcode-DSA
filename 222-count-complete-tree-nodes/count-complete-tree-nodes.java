71d999a0512574bda0c64fd2445bfd532f55bc36
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-04-10 22:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 49.6 MB (35.35%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -15,12 +15,21 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-      if(root==null)
-      {
-        return 0;
-      }  
-      int left=countNodes(root.left);
-      int right=countNodes(root.right);
-      return left+right+1;
+     int lh=0,rh=0;
+    TreeNode curr=root;
+     while(curr!=null)
+     {
+        lh++;
+        curr=curr.left;
+     }   
+     curr=root;
+     while(curr!=null)
+     {
+        rh++;
+        curr=curr.right;
+     }
+     if(lh==rh)
+     return (int)Math.pow(2,lh)-1;
+     return 1+countNodes(root.left)+countNodes(root.right);
     }
 }
