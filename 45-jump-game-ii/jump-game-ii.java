51603246db002215411a5840aa360236e01fce97
Priyanka Swaraj <priyanka.swaraj5@gmail.com>
2026-05-15 19:40:17 +0530

    Time: 379 ms (5.24%) | Memory: 47.3 MB (37.43%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+    int n=nums.length;
+    int[] dp=new int[n];
+    dp[0]=0;
+    for(int i=1;i<n;i++)
+    {
+        dp[i]=Integer.MAX_VALUE;
+    }    
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            if(nums[j]+j>=i)
+            {
+                if(dp[j]!=Integer.MAX_VALUE)
+                dp[i]=Math.min(dp[i],dp[j]+1);
+            }
+        }
+    }
+    return dp[n-1];
+    }
+}
